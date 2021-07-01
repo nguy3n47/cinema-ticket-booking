2c25ed5eed00d24003e334724d6239555850ed75
nguy3n47 <nguyen.fit.hcmus@outlook.com>
2021-07-01 14:35:53 +0700

    :white_check_mark: [Admin] - Done!

--- admin/src/App.js
+++ admin/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Switch>
           <Redirect exact from="/" to="/dashboard" />
           <Route path="/dashboard" component={Dashboard} />
-          <Route path="/auth/login" component={Login} />
+          <Route exact path="/auth/login" component={Login} />
           <Route component={NotFound} />
         </Switch>
       </Router>
